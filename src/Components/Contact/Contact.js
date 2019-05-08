43cd3f27974b935a4b9e74b793cea39ab4b4c421
Isaac Morerod <isaacmorerod@isaacsmcbookpro.attlocal.net>
2019-05-07 20:16:44 -0500

    need styling

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Contact</h2>
+                <p>Phone: 1-800-WE-REACT<br />
+                    Email: info@ReactActors.com
+</p>
+            </div>
+        );
+    }
+}
+
+export default Contact;
